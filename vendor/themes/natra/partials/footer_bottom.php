e114db2a88d93b4111c6f5b9fc7ec83c3412e85b
Andre Feri Saputra <andreferi135@gmail.com>
2024-07-31 23:47:21 +0700

    Update Terbaru

--- vendor/themes/natra/partials/footer_bottom.php
+++ vendor/themes/natra/partials/footer_bottom.php
@@ -13,10 +13,6 @@
       </div>
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
         <div class="footer_bottom_right">
-          &copy;
-          <a href="https://opendesa.id/" rel="noopener noreferrer" target="_blank">OpenDesa</a>
-          <i class="fa fa-circle" style="font-size: smaller;"></i>
-          <a href="https://github.com/OpenSID/OpenSID" rel="noopener noreferrer" target="_blank">OpenSID <?= AmbilVersi()?></a>
           <a href="<?= site_url(); ?>siteman" rel="noopener noreferrer" target="_blank"> | Natra <?= THEME_VERSION ?></a>
         </div>
       </div>
